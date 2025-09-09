bad50ccb6041598e5bf7c2e98a4d23a2e65fea1b
Ibrahim Kallungal <ibrahimayoob2131@gmail.com>
2025-09-09 12:28:46 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+  int a;
+  printf("Enter the number :");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+   printf("even");
+  }
+  else
+  {
+   printf("odd");
+  }
+ }
---
+++ sumoftwo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+ int a,b,s;
+ printf("Enter the two numbers");
+ scanf("%d %d",&a,&b);
+ s=a+b;
+ printf("sum of the numbers %d",s);
+}
